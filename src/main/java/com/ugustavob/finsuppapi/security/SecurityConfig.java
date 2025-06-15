8ce6a8a423f5bc2c6a9f4ef5b8b18236b2591b84
Gustavo Gabriel <123310218+uGustavoB@users.noreply.github.com>
2025-06-15 17:12:24 -0300

    Update SecurityConfig.java

--- src/main/java/com/ugustavob/finsuppapi/security/SecurityConfig.java
+++ src/main/java/com/ugustavob/finsuppapi/security/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200", "http://finsupp-v0.vercel.app", "http://finsupp.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200", "http://finsupp-v0.vercel.app", "http://finsupp.vercel.app", "http://finsupp-dev.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
